121e42df42992e7a7224cb851ad80801253a3b29
lifeistech <kaairiinn0913@yahoo.co.jp>
2020-09-08 01:46:48 +0900

    Initial Commit

---
+++ Stamp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stamp
+//
+//  Created by 井戸海里 on 2020/09/08.
+//  Copyright © 2020 IdoUmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
